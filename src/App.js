e4230f3c6e6397209e27b7b355a3a89a2ee947b0
Ashlyn Antoo <ashlynantoo@gmail.com>
2023-06-05 19:08:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import data from "./data";
+import Question from "./Question";
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <h3>questions and answers about login</h3>
+        <div>
+          {data.map((question) => {
+            return <Question key={question.id} {...question} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = (props) => {
+  const { title, info } = props;
+  const [isShowInfo, setIsShowInfo] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setIsShowInfo(!isShowInfo);
+          }}
+        >
+          {isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isShowInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Question;
